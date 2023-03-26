bb6d4810e1fb4133a2fca6e69be1c358dd0687ee
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-03-26 16:31:38 +0545

    feat: Add code to ask email and password

--- new_account.py
+++ new_account.py
@@ -9,7 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
+print("Enter your email or phone number")
+data = input()
+print("Enter your password")
+password = input()
+
 os.environ['PATH'] += r"C:\\driver"
 driver = webdriver.Firefox()
 driver.get("https://accounts.google.com/signin")
-
