6dbfe5f6e820927668370297c095126c96c09c68
Aamosh12 <123634110+Aamosh12@users.noreply.github.com>
2023-03-26 15:31:39 +0545

    feat: Add New python file

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
