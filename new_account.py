ecd44ea02e781bc1b23da33fc7fee7c5f5f01679
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-03-27 12:03:09 +0545

    feat: Add features that ask for info with user

--- new_account.py
+++ new_account.py
@@ -9,12 +9,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
-print("Enter your email or phone number")
-data = input()
-print("Enter your password")
+print("Enter you first name")
+first_name = input()
+print("Enter your last name")
+last_name = input()
+print("Enter username of your choice")
+username = input()
+print("Enter a strong password")
 password = input()
 
 os.environ['PATH'] += r"C:\\driver"
 driver = webdriver.Firefox()
 driver.get("https://accounts.google.com/signup")
-
